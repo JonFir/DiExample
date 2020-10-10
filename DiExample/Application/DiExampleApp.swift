6777469b2dae01028bfc5434062d58c961863c9d
jonfir <yolchev@gmsil.com>
2020-10-10 11:01:20 +0700

    first commit

---
+++ DiExample/Application/DiExampleApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct DiExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AddressFormScreen()
+        }
+    }
+}
---
+++ DiExample/Application/Domain/Providers/SelectProvider.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+protocol SelectProvider: AnyObject {
+    
+}
+
+class S: SelectProvider {
+    
+}
